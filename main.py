988f3b7fd953513938d8490cddfdb186f93ffea3
Senya <99828334+senyaGitHub@users.noreply.github.com>
2023-06-20 14:26:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,7 @@ from tkinter import ttk
 participants = []
 teams = []
 events = ["Volleyball", "Football", "Chess", "Ping-Pong", "Cooking competition"]
-event_points = {
-    "Volleyball": 20,
-    "Football": 20,
-}
 
-# Load data from JSON file
 def load_data():
     try:
         with open("data.json", "r") as file:
@@ -20,9 +15,12 @@ def load_data():
             participants.extend(data.get("participants", []))
             teams.extend(data.get("teams", []))
             events.extend(data.get("events", []))
-            event_points.update(data.get("event_points", {}))
     except FileNotFoundError:
-        pass
+        # Create a new data dictionary with empty fields
+        data = {"participants": [], "teams": [], "events": events}
+        with open("data.json", "w") as file:
+            json.dump(data, file)
+    return(data)
 
 # Save data to JSON file
 def save_data():
@@ -35,7 +33,7 @@ def register_individual():
     if len(participants) < 20:
         participant_name = individual_entry.get().strip()
         if participant_name:
-            participants.append({"Name": participant_name, "Scores": {}})
+            participants.append({"Name": participant_name, "Score": 0})
             messagebox.showinfo("Success", f"Individual participant '{participant_name}' has been registered.")
             save_data()
             update_individual_dropdown()
@@ -52,7 +50,7 @@ def register_team():
         member2 = member2_entry.get().strip()
 
         if team_name and member1 and member2:
-            teams.append({"Team Name": team_name, "Members": [member1, member2], "Scores": {}})
+            teams.append({"Team Name": team_name, "Members": [member1, member2], "Score": 0})
             messagebox.showinfo("Success", f"Team '{team_name}' has been registered.")
             save_data()
         else:
@@ -69,52 +67,39 @@ def enter_event_results():
         participant_name = participant_team.replace("Participant: ", "")
         for participant in participants:
             if participant["Name"].lower() == participant_name.lower():
-                participant["Score"] = score
+                participant["Score"] += score
                 break
     elif participant_team.startswith("Team"):
         team_name = participant_team.replace("Team: ", "")
         for team in teams:
             if team["Team Name"].lower() == team_name.lower():
-                team["Score"] = score
+                team["Score"] += score
                 break
 
-    messagebox.showinfo("Success", f"Score {score} assigned to {participant_team}.")
+    messagebox.showinfo("Success", f"Score {score} added to {participant_team}.")
     save_data()
 
+
 def display_details():
-    name = details_combobox.get()
-    found = False
-
-    # Search for team details
-    for team in teams:
-        if team["Team Name"].lower() == name.lower():
-            messagebox.showinfo("Team Details", f"Team Name: {team['Team Name']}\nMembers: {', '.join(team['Members'])}")
-            # Calculate and display total points for the team
-            total_points = calculate_total_points(team["Team Name"])
-            messagebox.showinfo("Team Details", f"Total Points: {total_points}")
-            found = True
-            break
-
-    # Search for individual participant details
-    if not found:
+    name = display_choice.get()
+    if name.startswith("Participant"):
+        participant_name = name.replace("Participant: ", "")
         for participant in participants:
-            if participant["Name"].lower() == name.lower():
-                messagebox.showinfo("Participant Details", f"Participant Name: {participant['Name']}")
-                found = True
-                break
+            if participant["Name"].lower() == participant_name.lower():
+                score = participant["Score"]
+                messagebox.showinfo("Success", f"Score {score} assigned to {participant_name}.")
+                return
+    elif name.startswith("Team"):
+        team_name = name.replace("Team: ", "")
+        for team in teams:
+            if team["Team Name"].lower() == team_name.lower():
+                score = team["Score"]
+                messagebox.showinfo("Success", f"Score {score} assigned to {team_name}.")
+                return
+    messagebox.showinfo("Error", "Invalid selection.")
 
-    if not found:
-        messagebox.showinfo("Error", "Participant or team not found.")
 
-def calculate_total_points(name):
-    total_points = 0
-    for event in events:
-        if event in event_points and event in participants[0]["Scores"]:
-            for participant in participants:
-                if event in participant["Scores"] and participant["Name"].lower() == name.lower():
-                    score = participant["Scores"][event]
-                    total_points += score * event_points[event]
-    return total_points
+
 def update_individual_dropdown():
     details_combobox['values'] = [participant["Name"] for participant in participants]
 
@@ -181,8 +166,10 @@ event_choice_label.pack()
 event_choice = ttk.Combobox(add_tab, values=events, state="readonly")
 event_choice.current(0)  # Default event choice
 event_choice.pack()
+
 score_label = ttk.Label(add_tab, text="Enter Score:")
 score_label.pack()
+
 participant_label = ttk.Label(add_tab, text="Select Participant/Team:")
 participant_label.pack()
 
@@ -191,20 +178,19 @@ participant_choice = ttk.Combobox(add_tab, values=[f"Participant: {participant['
                                   state="readonly")
 participant_choice.current(0)  # Default participant/team choice
 participant_choice.pack()
+
 score_entry = ttk.Entry(add_tab)
 score_entry.pack()
-
 
 results_button = ttk.Button(add_tab, text="Enter Results", command=enter_event_results)
 results_button.pack()
 
 # Display Details section
-details_label = ttk.Label(display_tab, text="Display Details")
-details_label.pack()
-
-details_combobox = ttk.Combobox(display_tab, values=[participant["Name"] for participant in participants], state="readonly")
-details_combobox.current(0)  # Default participant choice
-details_combobox.pack()
+display_choice = ttk.Combobox(display_tab, values=[f"Participant: {participant['Name']}" for participant in participants] +
+                                                 [f"Team: {team['Team Name']}" for team in teams],
+                                  state="readonly")
+display_choice.current(0)  # Default participant/team choice
+display_choice.pack()
 
 details_button = ttk.Button(display_tab, text="Display", command=display_details)
 details_button.pack()
